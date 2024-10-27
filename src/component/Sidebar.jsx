61e546bbdcdaf2857f2458a08e9b356f1a262cca
Natrin.Phasuksun <natrin.phasuksun@gmail.com>
2024-10-27 20:49:07 +0700

    first commit

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,218 +1,241 @@
 
 import { GoHome } from "react-icons/go";
 import { SiYoutubeshorts } from "react-icons/si";
-import { MdOutlineSubscriptions, MdWatchLater } from "react-icons/md";
-import { FaChevronRight } from "react-icons/fa";
-
-import { SiYoutubegaming } from "react-icons/si";
-import { FaRegNewspaper } from "react-icons/fa";
-import { MdHistory } from "react-icons/md";
-import { MdOutlinePlaylistPlay } from "react-icons/md";
+import { MdOutlineSubscriptions, MdHistory } from "react-icons/md";
+import { PiUserSquareThin } from "react-icons/pi";
+import { IoGameControllerOutline } from "react-icons/io5";
 import { AiOutlineLike } from "react-icons/ai";
-import { RiDownloadLine } from "react-icons/ri";
-import { LiaHotjar } from "react-icons/lia";
-import { PiMusicNoteLight } from "react-icons/pi";
-import { TfiCup } from "react-icons/tfi";
+import { FaChevronRight } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+import { SiYoutubestudio } from "react-icons/si";
 import { SiYoutubekids } from "react-icons/si";
+import { MdOutlineWatchLater } from "react-icons/md";
 import { SiYoutubemusic } from "react-icons/si";
-import { IoSettingsOutline } from "react-icons/io5";
-import { IoFlagOutline } from "react-icons/io5";
-import { CiCircleQuestion } from "react-icons/ci";
-import { GoReport } from "react-icons/go";
+import { SiTrendmicro } from "react-icons/si";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { PiFilmSlateLight } from "react-icons/pi";
+import { CgMediaLive } from "react-icons/cg";
+import { FaRegNewspaper } from "react-icons/fa";
+import { TfiCup } from "react-icons/tfi";
+import { PiLightbulbLight } from "react-icons/pi";
+import { SiStylelint } from "react-icons/si";
+import { MdPodcasts } from "react-icons/md";
+import { BiVideo } from "react-icons/bi";
 function Sidebar() {
-    const sidebarItems =[
-        {
-            id:1,
-            name:"HOME",
-            icon:<GoHome/>
-        },
-        {
-            id:2,
-            name:"Shorts",
-            icon:<SiYoutubeshorts/>
-        },
-        {
-            id:3,
-            name:"Subscribe",
-            icon:<MdOutlineSubscriptions/>
-        },
-        
-    ];
-    const sidebarItems2 = [ 
-        {
-            id:1,
-            name:"History",
-            icon:<MdHistory />
-        },
-        {
-            id:2,
-            name:"Playlists",
-            icon:<MdOutlinePlaylistPlay/>
-        },
-        {
-            id:3,
-            name:"Like Videos",
-            icon:<AiOutlineLike/>
-        },
-        {
-            id:4,
-            name:"Watch Later",
-            icon:<MdWatchLater/>
-        },
-        {
-            id:5,
-            name:"Download Videos",
-            icon:<RiDownloadLine/>
-        },
-    ];
-    const sidebarItems3 = [ 
-        {
-            id:1,
-            name:"Trending",
-            icon:<LiaHotjar />
-        },
-        {
-            id:2,
-            name:"Music",
-            icon:<PiMusicNoteLight/>
-        },
-        {
-            id:3,
-            name:"Game",
-            icon:<SiYoutubegaming/>
-        },
-        {
-            id:4,
-            name:"News",
-            icon:<FaRegNewspaper/>
-        },
-        {
-            id:5,
-            name:"Sport",
-            icon:<TfiCup/>
-        },
-    ];
-    const sidebarItems4 = [ 
-        {
-            id:1,
-            name:"Youtube Music",
-            icon:<SiYoutubemusic />
-        },
-        {
-            id:2,
-            name:"Youtube Kids",
-            icon:<SiYoutubekids/>
-        },
-    ];
-    const sidebarItems5 = [ 
-        {
-            id:1,
-            name:"Setting",
-            icon:<IoSettingsOutline />
-        },
-        {
-            id:2,
-            name:"Report",
-            icon:<IoFlagOutline/>
-        },
-        {
-            id:3,
-            name:"Youtube Music",
-            icon:< CiCircleQuestion />
-        },
-        {
-            id:4,
-            name:"Comment",
-            icon:<GoReport/>
-        },
-    ];
+  const sidebarItems = [
+    {
+      id: 1,
+      name: "Home",
+      icon: <GoHome />,
+    },
+    {
+      id: 2,
+      name: "Shorts",
+      icon: <SiYoutubeshorts />,
+    },
+    {
+      id: 3,
+      name: "Subscriptions",
+      icon: <MdOutlineSubscriptions />,
+    },
+  ];
+  const sidebarItems2 = [
+    {
+      id: 1,
+      name: "Your Channel",
+      icon: <PiUserSquareThin />,
+    },
+    {
+      id: 2,
+      name: "History",
+      icon: <MdHistory />,
+    },
+    {
+      id: 3,
+      name: "Playlists",
+      icon: <MdOutlineSubscriptions />,
+    },
+    {
+      id: 4,
+      name: "Your Videos",
+      icon: <BiVideo />,
+    },
+    {
+      id: 5,
+      name: "Watch later",
+      icon: <MdOutlineWatchLater />,
+    },
+    {
+      id: 6,
+      name: "Liked videos",
+      icon: <AiOutlineLike />,
+    },
+  ];
+  const sidebarItems3 = [
+    {
+      id: 1,
+      name: "Trending",
+      icon: <SiTrendmicro />,
+    },
+    {
+      id: 2,
+      name: "Shopping",
+      icon: <HiOutlineShoppingBag />,
+    },
+    {
+      id: 3,
+      name: "Music",
+      icon: <SiYoutubemusic />,
+    },
+    {
+      id: 4,
+      name: "Films",
+      icon: <PiFilmSlateLight />,
+    },
+    {
+      id: 5,
+      name: "Live",
+      icon: <CgMediaLive />,
+    },
+    {
+      id: 6,
+      name: "Gaming",
+      icon: <IoGameControllerOutline />,
+    },
+    {
+      id: 7,
+      name: "News",
+      icon: <FaRegNewspaper />,
+    },
+    {
+      id: 8,
+      name: "Sport",
+      icon: <TfiCup />,
+    },
+    {
+      id: 9,
+      name: "Courses",
+      icon: <SiStylelint />,
+    },
+    {
+      id: 10,
+      name: "Fashion & beauty",
+      icon: <PiLightbulbLight />,
+    },
+    {
+      id: 11,
+      name: "Padcasts",
+      icon: <MdPodcasts />,
+    },
+  ];
+  const sidebarItems4 = [
+    {
+      id: 1,
+      name: "Youtube Premium",
+      icon: <FaYoutube />,
+    },
+    {
+      id: 2,
+      name: "Youtube Studio",
+      icon: <SiYoutubestudio />,
+    },
+    {
+      id: 3,
+      name: "Youtube Music",
+      icon: <SiYoutubemusic />,
+    },
+    {
+      id: 4,
+      name: "Youtube Kids",
+      icon: <SiYoutubekids />,
+    },
+  ];
   return (
-    <div className="px-6 w-[17%] h-[calc(100vh-6.625rem)] overflow-y-scroll scrollbar-thin overflow-x-hidden mt-16 " >
-        {/* Home */}
-        <div className="space-y-3  items-center">
-           {sidebarItems.map((item) => {
-            return (
-                    <div key={item.id} className="flex items-center space-x-6 hover:bg-gray-300 duration-300 rounded-xl p-1">
-                        <div className="text-xl cursor-pointer">{item.icon}</div>
-                        <span className="cursor-pointer">{item.name}</span>                    
-                    </div>                   
-            );
-        })}           
-        </div>
-        <br/>
-        <hr/>
-        {/* You */}
-        <div className="mt-4 space-y-3  items-center">
-            <div className="flex items-center space-x-2">
-                <h1 className="font-semibold">You</h1>
-                <FaChevronRight/>
+    <div className="px-6 w-[17%] h-[calc(100vh-6.625rem)] overflow-y-scroll overflow-x-hidden ">
+      {/* Home */}
+      <div className=" space-y-3 items-center">
+        {sidebarItems.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="flex items-center space-x-6 hover:bg-gray-300 duration-300 rounded-xl p-1"
+            >
+              <div className="text-xl cursor-pointer">{item.icon}</div>
+              <span className="cursor-pointer">{item.name}</span>
             </div>
-           {sidebarItems2.map((item) => {
-            return (
-                    <div key={item.id} className="flex items-center space-x-6 hover:bg-gray-300 duration-300 rounded-xl p-1">
-                        <div className="text-xl cursor-pointer">{item.icon}</div>
-                        <span className="cursor-pointer">{item.name}</span>                    
-                    </div>                   
-            );
-        })}           
+          );
+        })}
+      </div>
+      <br />
+      <hr />
+      {/* You */}
+      <div className="mt-4 space-y-3 items-center">
+        <div className="flex items-center space-x-2">
+          <h1>You</h1>
+          <FaChevronRight />
         </div>
-        <br/>
-        <hr/>
-        {/* Explore */}
-        <div className="mt-4 space-y-3  items-center">
-            <div className="flex items-center space-x-2">
-                <h1 className="font-semibold">Explore</h1>
+        {sidebarItems2.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="flex items-center space-x-6 hover:bg-gray-300 duration-300 rounded-xl p-1"
+            >
+              <div className="text-xl cursor-pointer">{item.icon}</div>
+              <span className="cursor-pointer">{item.name}</span>
             </div>
-           {sidebarItems3.map((item) => {
-            return (
-                    <div key={item.id} className="flex items-center space-x-6 hover:bg-gray-300 duration-300 rounded-xl p-1">
-                        <div className="text-xl cursor-pointer">{item.icon}</div>
-                        <span className="cursor-pointer">{item.name}</span>                    
-                    </div>                   
-            );
-        })}           
+          );
+        })}
+      </div>
+      <br />
+      <hr />
+      {/* Explore */}
+      <div className="mt-4 space-y-3 items-center">
+        <div className="items-center space-x-2">
+          <h1 className=" font-semibold">Explore</h1>
         </div>
-        <br/>
-        <hr/>
-        {/* More from Youtube */}
-        <div className="mt-4 space-y-3  items-center">
-            <div className="flex items-center space-x-2">
-                <h1 className="font-semibold">More Youtube</h1>
+        {sidebarItems3.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="flex items-center space-x-6 hover:bg-gray-300 duration-300 rounded-xl p-1"
+            >
+              <div className="text-xl cursor-pointer">{item.icon}</div>
+              <span className="cursor-pointer">{item.name}</span>
             </div>
-           {sidebarItems4.map((item) => {
-            return (
-                    <div key={item.id} className="flex items-center space-x-6 hover:bg-gray-300 duration-300 rounded-xl p-1">
-                        <div className="text-xl cursor-pointer text-red-600">{item.icon}</div>
-                        <span className="cursor-pointer">{item.name}</span>                    
-                    </div>                   
-            );
-        })}           
+          );
+        })}
+      </div>
+      <br />
+      <hr />
+      {/* More section */}
+      <div className="mt-4 space-y-3 items-center">
+        <div className="items-center space-x-2">
+          <h1 className=" font-semibold">More From Youtube</h1>
         </div>
-        <br/>
-        <hr/>
-        {/* Setting */}
-        <div className="mt-4 space-y-3  items-center">
-           {sidebarItems5.map((item) => {
-            return (
-                    <div key={item.id} className="flex items-center space-x-6 hover:bg-gray-300 duration-300 rounded-xl p-1">
-                        <div className="text-xl cursor-pointer">{item.icon}</div>
-                        <span className="cursor-pointer">{item.name}</span>                    
-                    </div>                   
-            );
-        })}  
-        <hr/>         
-        </div>
-        <span className="text-xs font-semibold text-gray-700">
-        เกี่ยวกับ สื่อลิขสิทธิ์ ติดต่อเรา <br/> 
-        ครีเอเตอร์ โฆษณา <br/> 
-        นักพัฒนาซอฟต์แวร์ <br/>
-        <p>ข้อกำหนดความเป็นส่วน <br/>
-        ตัวนโยบายและความปลอดภัย <br/>
-        วิธีการทำงานของ YouTube<br/>
-        ทดลองใช้ฟีเจอร์ใหม่</p>
-        </span>
-        <br/>
-        <p className="text-xs font-semibold text-gray-400"> &copy; 2024 Google LLC</p>
+        {sidebarItems4.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="flex items-center space-x-6 hover:bg-gray-300 duration-300 rounded-xl p-1"
+            >
+              <div className="text-xl cursor-pointer text-red-500">
+                {item.icon}
+              </div>
+              <span className="cursor-pointer">{item.name}</span>
+            </div>
+          );
+        })}
+        <hr />
+      </div>
+      <br />
+      <span className="text-xs text-gray-600 font-semibold">
+        About Press Copyright <br /> Contact us Creators <br /> Advertise
+        Developers <br />
+        <p className="mt-3">Terms Privacy Policy & Safety</p> How YouTube works{" "}
+        <br /> Test new features
+      </span>
+      <br />
+      <p className="text-xs text-gray-500 mt-3">© 2024 Learn Coding</p>
     </div>
   );
 }
